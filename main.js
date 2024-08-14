a598dbcc276275c0104e9bff91372c924e033f7e
prodigy3211 <37477839+Prodigy3211@users.noreply.github.com>
2024-08-13 21:49:40 -0400

    Add new files for project
    Added json and main.js file with some basic scraper code.

---
+++ main.js
@@ -0,0 +1,15 @@
+const { default: puppeteer } = require("puppeteer")
+
+async function scrapeProduct(url){
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const [el] = await page.$x('//*[@id="root"]/div/div/div[2]/div/div/div/div[1]/div/main/div[1]/div[1]/div[2]/div[2]/div[1]/div[6]/div/h1//*[@id="imgBlkFont"]');
+    const src = await el.getProperty('src');
+    const srcTxt = await src.jsonValue();
+
+    console.log(srcTxt);
+}
+
+scrapeProduct('https://www.eventbrite.com/e/afro-caribbean-rooftop-party-tickets-143005871411?aff=ebdssbdestsearch')
